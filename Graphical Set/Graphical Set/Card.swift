b5d6008c9ad7a9ce071f11e062330a00abc33b11
Marwan Salama <marwansalama94@gmail.com>
2019-10-24 22:09:09 +0200

    copying model from previous hw

---
+++ Graphical Set/Graphical Set/Card.swift
@@ -0,0 +1,78 @@
+//
+//  Card.swift
+//  Set HW2
+//
+//  Created by Marwan Salama on 10/16/19.
+//  Copyright © 2019 Marwan Salama. All rights reserved.
+//
+
+import Foundation
+
+struct Card: CustomStringConvertible{
+    let pipsCount:Int
+    let color : Color
+    let shade : Shade
+    let shape : Shape
+    
+    var description: String {return "\(shape),\(shade),\(color),pips:\(pipsCount)"}
+    
+    
+    enum Color: CustomStringConvertible{
+        case red
+        case green
+        case purple
+        
+        static var all = [Color.red, Color.green, Color.purple]
+        var description: String {
+            switch self{
+            case .red: return "red"
+            case .green: return "green"
+            case .purple: return "purple"
+            }
+        }
+    }
+    
+    enum Shade: CustomStringConvertible {
+        case stroke
+        case fill
+        case empty
+        
+        static var all = [Shade.stroke, Shade.fill, Shade.empty]
+        
+        var description: String{
+            switch self{
+            case .empty: return "empty"
+            case .stroke: return "stroke"
+            case .fill: return "fill"
+            }
+        }
+    }
+    
+    enum Shape: CustomStringConvertible{
+        case square
+        case triangle
+        case circle
+        
+        static var all = [Shape.square,Shape.triangle,Shape.circle]
+        var description: String{
+            switch self{
+            case .circle: return "circle"
+            case .triangle : return "triangle"
+            case .square : return "square"
+            }
+        }
+    }
+    
+    
+}
+
+extension Card: Equatable{
+    static func == (lhs: Card, rhs: Card) -> Bool {
+        return
+            lhs.pipsCount == rhs.pipsCount  &&
+            lhs.shape == rhs.shape &&
+            lhs.shade == rhs.shade  &&
+            lhs.color == rhs.color
+    }
+    
+}
---
+++ Graphical Set/Graphical Set/setGame.swift
@@ -0,0 +1,109 @@
+//
+//  setGame.swift
+//  Set HW2
+//
+//  Created by Marwan Salama on 10/17/19.
+//  Copyright © 2019 Marwan Salama. All rights reserved.
+//
+
+import Foundation
+
+struct setGame{
+    private var cards = [Card]()
+    private(set) var playingCards = [Card]()
+    private(set) var selectedCards = [Card]()
+    
+    mutating func CreateDeckOfCards(){
+        for pips in 1..<4{
+            for shade in Card.Shade.all{
+                for color in Card.Color.all{
+                    for shape in Card.Shape.all{
+                        let card = Card(pipsCount: pips, color: color, shade: shade, shape: shape)
+                        cards.append(card)
+                    }
+                }
+            }
+        }
+    }
+    
+    mutating func deal3MoreCards(){
+        if cards.count >= 3{
+            for _ in 0..<3{
+                playingCards.append(cards.remove(at: 0))
+            }
+        }
+    }
+    
+    private func firstMatchingRule(firstCard: Card,secondCard: Card,thirdCard: Card)-> Bool{
+        return (
+            (firstCard.shape == secondCard.shape && secondCard.shape == thirdCard.shape) &&
+            (firstCard.color == secondCard.color && secondCard.color == thirdCard.color) &&
+            (firstCard.pipsCount == secondCard.pipsCount && secondCard.pipsCount == thirdCard.pipsCount) &&
+            (firstCard.shade != secondCard.shade && firstCard.shade != thirdCard.shade && secondCard.shade != thirdCard.shade))
+    }
+    
+    private func secondMatchingRule(firstCard: Card,secondCard: Card,thirdCard: Card)-> Bool{
+        return (
+            (firstCard.shade == secondCard.shade && secondCard.shade == thirdCard.shade) &&
+            (firstCard.shape != secondCard.shape && firstCard.shape != thirdCard.shape && secondCard.shape != thirdCard.shape) &&
+            (firstCard.color != secondCard.color && firstCard.color != thirdCard.color && secondCard.color != thirdCard.color) &&
+            (firstCard.pipsCount != secondCard.pipsCount && firstCard.pipsCount != thirdCard.pipsCount && secondCard.pipsCount != thirdCard.pipsCount)
+        )
+    }
+    
+    private func thirdMatchingRule(firstCard: Card,secondCard: Card,thirdCard: Card)-> Bool{
+        return (
+            (firstCard.shade != secondCard.shade && firstCard.shade != thirdCard.shade && secondCard.shade != thirdCard.shade) &&
+            (firstCard.shape != secondCard.shape && firstCard.shape != thirdCard.shape && secondCard.shape != thirdCard.shape) &&
+            (firstCard.color != secondCard.color && firstCard.color != thirdCard.color && secondCard.color != thirdCard.color) &&
+            (firstCard.pipsCount != secondCard.pipsCount && firstCard.pipsCount != thirdCard.pipsCount && secondCard.pipsCount != thirdCard.pipsCount)
+        )
+    }
+    
+    
+    private func PotentialMatch() -> Bool{
+        let firstCard = selectedCards[0]
+        let secondCard = selectedCards[1]
+        let thirdCard = selectedCards[2]
+        /* Matching rules
+         1- same shape, same color,same number of symbols and different shading
+         2- different shapes, different colors, same shading , different number of symbols
+         3- different shapes, different colors, different shading and different number of symbols
+         */
+        return firstMatchingRule(firstCard: firstCard, secondCard: secondCard, thirdCard: thirdCard) ||
+            secondMatchingRule(firstCard: firstCard, secondCard: secondCard, thirdCard: thirdCard) ||
+            thirdMatchingRule(firstCard: firstCard, secondCard: secondCard, thirdCard: thirdCard)
+        
+    }
+    
+    mutating func selectCard(at index:Int){
+
+        let selectedCard = playingCards[index]
+        if selectedCards.count < 3{
+            if !selectedCards.contains(selectedCard){
+                selectedCards.append(selectedCard)
+            }else{
+                selectedCards.remove(at: selectedCards.firstIndex(of: selectedCard)!)
+            }
+        }else{
+            if PotentialMatch(){
+                for card in selectedCards{
+                    playingCards.remove(at: playingCards.firstIndex(of: card)!)
+                }
+                if !cards.isEmpty{
+                    deal3MoreCards()
+                }
+            }
+            selectedCards.removeAll()
+            selectedCards.append(selectedCard)
+        }
+    }
+    
+    init(){
+        CreateDeckOfCards()
+        cards = cards.shuffled()
+        for _ in 0..<4{
+            deal3MoreCards()
+        }
+    }
+}
